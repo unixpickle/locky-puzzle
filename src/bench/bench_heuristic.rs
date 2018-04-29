4bcb75b26dd178257f2bf7f38259aa4dc8e285d5
Alex Nichol <unixpickle@gmail.com>
2018-04-28 17:44:48 -0700

    benchmark LockProj

--- src/bench/bench_heuristic.rs
+++ src/bench/bench_heuristic.rs
@@ -4,7 +4,7 @@ extern crate locky_puzzle;
 
 use std::time::Instant;
 
-use locky_puzzle::{ArrowAxisProj, CornerProj, Proj, ProjHeuristic};
+use locky_puzzle::{ArrowAxisProj, CornerProj, LockProj, Proj, ProjHeuristic};
 
 fn main() {
     time_heuristic::<ArrowAxisProj>("ArrowAxisProj(5)", 5);
@@ -14,6 +14,13 @@ fn main() {
     time_heuristic::<CornerProj>("CornerProj(5)", 5);
     time_heuristic::<CornerProj>("CornerProj(6)", 6);
     time_heuristic::<CornerProj>("CornerProj(7)", 7);
+
+    time_heuristic::<LockProj>("LockProj(5)", 5);
+    time_heuristic::<LockProj>("LockProj(6)", 6);
+    time_heuristic::<LockProj>("LockProj(7)", 7);
+    time_heuristic::<LockProj>("LockProj(8)", 8);
+    time_heuristic::<LockProj>("LockProj(9)", 9);
+    time_heuristic::<LockProj>("LockProj(10)", 10);
 }
 
 fn time_heuristic<T: Proj>(label: &str, depth: u8) {
